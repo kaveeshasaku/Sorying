e87bc09358314b0555e9413a09580db7c05ae073
kaveeshasaku <dissanayakekaveesha59@gmail.com>
2024-06-08 12:47:48 +0530

    Stack Push

---
+++ StackPush.java
@@ -0,0 +1,27 @@
+package DataStructures;
+
+import java.util.Stack;
+
+public class StackPush {
+	
+		public static void main(String[] args)
+		{
+
+			
+			Stack<String> stack = new Stack<>();
+
+		
+			stack.push("4");
+			stack.push("16");
+			stack.push("28");
+			stack.push("78");
+			stack.push("90"); 
+
+		
+			System.out.println("Push Stack: " + stack);
+			
+		}
+	}
+
+
+
